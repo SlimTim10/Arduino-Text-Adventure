6cad16b28b18b5b6449ad6f2d6fe2129b7e52405
SlimTim10 <slimtim10@gmail.com>
2015-02-09 08:25:27 -0500

    Change nokia5110 library to C. Fix minor warnings.

--- nokia5110.h
+++ nokia5110.h
@@ -3,6 +3,10 @@
 
 #include <Arduino.h>
 
+#ifdef __cplusplus
+extern "C" {	// Use C library with Arduino
+#endif
+
 enum {
 	LCD_C = LOW,
 	LCD_D = HIGH,
@@ -16,7 +20,11 @@ enum screen {
 void lcdCharacter(char);
 void lcdClear(void);
 void lcdInitialize(void);
-void lcdString(char *);
+void lcdString(char const *);
 void lcdWrite(byte, byte);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif
