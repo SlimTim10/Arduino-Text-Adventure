9689fe9ff429bde04b0cc3f8eac50fa4a5033fad
SlimTim10 <slimtim10@gmail.com>
2015-02-10 17:34:27 -0500

    Minor changes.

--- arduinotextadventure.ino
+++ arduinotextadventure.ino
@@ -17,22 +17,6 @@ void setup(void) {
    	lcd_light(0);
 
 	game_intro();
-
-	/* lcd_write_wrap("Welcome to the text adventure", 0, 0); */
-	/* lcd_write(" Up", 0, 3); */
-	/* lcd_write(" Down", 40, 3); */
-	/* lcd_write(" Left", 0, 4); */
-	/* lcd_write(" Right", 40, 4); */
-	/* lcd_write(">", 0, 3); */
-	/* delay(2000); */
-	/* lcd_write(" ", 0, 3); */
-	/* lcd_write(">", 40, 3); */
-	/* delay(2000); */
-	/* lcd_write(" ", 40, 3); */
-	/* lcd_write(">", 0, 4); */
-	/* delay(2000); */
-	/* lcd_write(" ", 0, 4); */
-	/* lcd_write(">", 40, 4); */
 }
 
 void loop(void) {
